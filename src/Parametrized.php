db60f09dd053ab68509d4740f4f6a0f8becbaab1
Valnar Dewan <mail@story75.com>
2015-06-03 08:38:53 +0200

    [TASK] Move old traits into package

---
+++ src/Parametrized.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Bonefish\Traits;
+
+/**
+ * Copyright (C) 2014  Alexander Schmidt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * @author     Alexander Schmidt <mail@story75.com>
+ * @copyright  Copyright (c) 2015, Alexander Schmidt
+ * @version    1.0
+ * @date       2015-01-28
+ * @package Bonefish\Traits
+ */
+trait Parametrized
+{
+    /**
+     * @var array
+     */
+    protected $parameters = array();
+
+    /**
+     * @param array $parameters
+     */
+    public function setParameters(array $parameters = array())
+    {
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * @return array
+     */
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
+} 
